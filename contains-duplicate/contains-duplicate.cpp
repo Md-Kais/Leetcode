ae8f8db469a6a8d38e38ecfc340b557ddba09206
Kais <62563474+Md-Kais@users.noreply.github.com>
2021-07-28 03:19:40 +0600

    Time: 44 ms (31.14%), Space: 22.4 MB (6.52%) - LeetHub

---
+++ contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int>s;
+        for(int i=0;i<nums.size();i++){
+             s.insert(nums[i]);
+        }
+        if(s.size()==nums.size()){
+            return false;
+        }
+        else{
+            return true;
+        }
+       
+    }
+};
