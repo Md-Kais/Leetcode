c07001f7d2b41cb1eb3c22e984b8c3d878390518
Kais <62563474+Md-Kais@users.noreply.github.com>
2021-07-28 21:30:43 +0600

    Time: 4 ms (73.17%), Space: 7.2 MB (50.52%) - LeetHub

---
+++ beautiful-array/beautiful-array.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> beautifulArray(int N)
+    {
+        vector<int> res = {1};
+        while (res.size() < N)
+        {
+            //make another array to store temporary array.
+            vector<int> v;
+            //take odd; (2*n-1)
+            for (auto e : res)
+            {
+                if (2 * e - 1 <= N)
+                    v.push_back(2 * e - 1);
+            }
+            //take even;(2*n)
+            for (auto e : res)
+            {
+                if (2 * e <= N)
+                    v.push_back(2 * e );
+            }
+            res=v;
+        }
+        return res;
+    }
+};
