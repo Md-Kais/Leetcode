5942205794df427e985cc2a14b373ed699210325
Kais <62563474+Md-Kais@users.noreply.github.com>
2021-07-29 03:26:37 +0600

    Time: 3 ms (33.18%), Space: 5.7 MB (87.78%) - LeetHub

---
+++ power-of-four/power-of-four.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    bool isPowerOfFour(int n)
+    {
+      if(n==0) return false;
+        else{
+              int rightmost_bit = 32 - __builtin_clz(n);
+        if (__builtin_popcount(n) == 1)
+        {
+            if(rightmost_bit % 4 ==1 || rightmost_bit % 4==3){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            return false;
+        }
+        }
+    }
+};
