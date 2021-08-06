3fdd52fb4b2fd06865d4809a767d522580c58cde
Kais <62563474+Md-Kais@users.noreply.github.com>
2021-08-06 22:23:51 +0600

    Runtime: 16 ms, faster than 50.29% of C++ online submissions for Two Sum.

--- two-sum/two-sum.cpp
+++ two-sum/two-sum.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
+   
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int>v;
-        for(int i=0;i<nums.size()-1;i++){
-            for(int j=i+1;j<nums.size();j++){
-            if(nums[i]+nums[j]==target) {
-                    v.push_back(i);
-                    v.push_back(j);
-                   return v;
-            }
-                }
-            }
+        multimap<int,int>m;
+        int i=0;
         
+        for(auto e: nums){
+            m.insert({e,i});
+            i++;
+        }
+        sort(nums.begin(),nums.end());
+        vector<int>v;
+       int start=0;
+       int end=nums.size()-1;
        
+        
+        while(start<end){
+            if(nums[start]+nums[end]==target){
+                   auto it = m.find(nums[start]);
+                    v.push_back(it->second);
+                    m.erase(it);
+                        it = m.find(nums[end]);
+                     v.push_back(it->second);
+            
+            }
+            if(nums[start]+nums[end]<target){
+                start++;
+            }
+            else{
+                end--;
+            }
+        }
         return v;
+       
+      
     }
 };
