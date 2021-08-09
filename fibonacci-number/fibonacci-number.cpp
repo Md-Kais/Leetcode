cf95a8126d20720be3d175d0811ea2a8cdca83fe
Kais <62563474+Md-Kais@users.noreply.github.com>
2021-08-09 13:22:31 +0600

    Time: 16 ms (10.70%), Space: 5.9 MB (77.55%) - LeetHub

---
+++ fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    long long ans=1;
+    int fib(int n) {
+        if(n==0) return 0;
+        if(n==1) return 1;
+      
+        return fib(n-2)+fib(n-1);
+        
+    }
+};
