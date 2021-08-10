a2653dc82797fd2c244239a0468a816650673abe
Kais <62563474+Md-Kais@users.noreply.github.com>
2021-08-10 22:27:25 +0600

    Time: 16 ms (22.91%), Space: 5.9 MB (26.21%) - LeetHub

---
+++ bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
@@ -0,0 +1,23 @@
+
+class Solution
+{
+public:
+    int rangeBitwiseAnd(int left, int right)
+    {
+        long long int ans;
+        if(left==0){ ans=0; }
+        else if (__builtin_clz(left) == __builtin_clz(right))
+        {
+            ans = left;
+            for (long long int i = left; i <= right; i++)
+            {
+                ans &= i;
+            }
+        }
+        else
+        {
+            ans = 0;
+        }
+        return ans;
+    }
+};
