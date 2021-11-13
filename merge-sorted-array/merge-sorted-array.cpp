af21db9d18e96759d6a578c772d456164c8eb10c
Kais <markkais129@gmail.com>
2021-11-13 20:26:28 +0600

    Update merge-sorted-array.cpp

--- merge-sorted-array/merge-sorted-array.cpp
+++ merge-sorted-array/merge-sorted-array.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int erased=nums1.size()-m;
-        nums1.insert(nums1.end(),nums2.begin(),nums2.end());
-        sort(nums1.begin(),nums1.end());
-        for(auto e: nums1) cout<<e<<" ";
-    
-        cout<<erased<<endl;
-        auto zero=std::find(nums1.begin(),nums1.end(),0);
-        nums1.erase(zero,zero+erased);
-        
+        //using two pointer tech
+        int on=m-1,two=n-1,i=nums1.size()-1;
+        while(two>=0) {
+            if(on>=0 && nums1[on]>=nums2[two]){
+                nums1[i--]=nums1[on--];
+            }
+            else{
+                  nums1[i--]=nums2[two--];
+            }
+        }
     }
 };
