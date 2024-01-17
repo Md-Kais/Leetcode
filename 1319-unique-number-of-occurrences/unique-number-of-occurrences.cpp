e5e57e537dfcfc107f9babc61976e78975b55198
Kais <markkais129@gmail.com>
2024-01-17 18:52:42 +0600

    Time: 3 ms (54.98%) | Memory: 9.2 MB (9.43%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        bool ans=true;
+        set<int>s;
+        unordered_set<int>ss;
+        map<int,int>m;
+        for(auto e: arr){
+            m[e]++;
+            s.insert(e);
+        }
+        for(auto e: s){
+            int x= m[e];
+            cout<<x<<endl;
+            if(ss.count(x)==1){
+                ans=false;
+            }
+            ss.insert(x);
+        }
+        return ans;
+
+    }
+};
