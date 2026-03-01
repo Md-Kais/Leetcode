c091373a9c45781fd8f9b740e18ce192ca58ab7f
Md. Kais <markkais129@gmail.com>
2026-03-01 07:06:24 +0600

    Easy 0485-max-consecutive-ones 0 ms (100%) 50.2 MB (33.14%) 03-01-2026

---
+++ LeetCode/Easy/0485-max-consecutive-ones/0485-max-consecutive-ones-03-01-2026-07-06-23.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int ans=0,temp=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==1) temp++;
+            else temp=0;
+            ans=max(temp,ans);
+        }
+        return ans;
+    }
+};
