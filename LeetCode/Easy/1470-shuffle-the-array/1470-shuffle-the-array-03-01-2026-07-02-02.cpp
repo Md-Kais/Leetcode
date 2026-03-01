9723661c7d3918196545484318ed3e36565ce25d
Md. Kais <markkais129@gmail.com>
2026-03-01 07:02:03 +0600

    Easy 1470-shuffle-the-array 8 ms (16.98%) 13.5 MB (17.51%) 03-01-2026

---
+++ LeetCode/Easy/1470-shuffle-the-array/1470-shuffle-the-array-03-01-2026-07-02-02.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int>ans(2*n,0);
+        int k=0;
+        for(int i=0;i<n;i++){
+            ans[k]=nums[i];
+            ans[k+1]=nums[i+n];
+            k+=2;
+            
+        }
+        return ans;
+    }
+};
